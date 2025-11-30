c6a6b80b046d1979beb78907acab202e63881dc7
lonelyking007 <22ucs065@lnmiit.ac.in>
2025-11-30 11:24:34 +0530

    spring project initialized

---
+++ src/main/java/com/example/Trade_Craft/TradeCraftApplication.java
@@ -0,0 +1,13 @@
+package com.example.Trade_Craft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeCraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Trade_Craft/TradeCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Trade_Craft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeCraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
